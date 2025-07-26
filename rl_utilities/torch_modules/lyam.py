9e47e734c019a2b27726d2a7c520703d81ad9229
Matt Allen <piemanfour@gmail.com>
2025-07-25 23:20:30 -0400

    Adjust LyAm.

--- rl_utilities/torch_modules/lyam.py
+++ rl_utilities/torch_modules/lyam.py
@@ -5,8 +5,8 @@ class LyAm(Optimizer):
     """
     LyAm optimizer. From the paper "LyAm: Robust Non-Convex Optimization for Stable Learning in Noisy Environments". Preprint here: https://arxiv.org/abs/2507.11262v1.
     """
-    def __init__(self, params, lr=3e-4, betas=(0.9, 0.999), eps=1e-8):
-        defaults = dict(lr=lr, betas=betas, eps=eps)
+    def __init__(self, params, lr=0.003, betas=(0.9, 0.99)):
+        defaults = dict(lr=lr, betas=betas)
         super().__init__(params, defaults)
 
     @torch.no_grad()
@@ -15,10 +15,10 @@ class LyAm(Optimizer):
         if closure is not None:
             with torch.enable_grad():
                 loss = closure()
+
         for group in self.param_groups:
             beta1, beta2 = group['betas']
             lr = group['lr']
-            eps = group['eps']
             for p in group['params']:
                 if p.grad is None:
                     continue
@@ -41,6 +41,7 @@ class LyAm(Optimizer):
                 m_hat = exp_avg / bias_correction1
                 v_hat = exp_avg_sq / bias_correction2
                 step_size = lr / (1 + v_hat)
+                m_hat.mul_(step_size)
                 
-                p.data.add_(m_hat.mul_(step_size), alpha=-1)
+                p.data.add_(m_hat, alpha=-1)
         return loss
